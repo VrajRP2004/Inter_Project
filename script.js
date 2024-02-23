5491d2fd2c5458be62d6911afecc3179e8621f3f
Vraj Prajapati <106799601+VrajRP2004@users.noreply.github.com>
2024-02-23 16:36:08 +0530

    add file added
    a

---
+++ script.js
@@ -0,0 +1,20 @@
+// function handleclick(){
+//     var popupWindow = window.open("popup.html", "Popup", "width=400,height=300");
+// }
+function toggleDropdown(dropdownOptions) {
+    var dropdownOptions = document.getElementById(dropdownOptions);
+    dropdownOptions.style.display = dropdownOptions.style.display === "block" ? "none" : "block";
+}
+function toggleSliceDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+   
+}
+function toggleSliceDropdown1(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+
+}
+function vraj(){
+    console.log('vraj')
+}
